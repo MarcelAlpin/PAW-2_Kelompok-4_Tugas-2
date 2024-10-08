1ffa1c14164fe374e95b530f349eda570f2e1ea2
oliviaaja1 <olivia_2226240023@mhs.mdp.ac.id>
2024-10-08 11:11:44 +0700

    contact

--- Tugas-2/app.js
+++ Tugas-2/app.js
@@ -8,6 +8,7 @@ const expressLayouts = require("express-ejs-layouts");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var contactRouter =  require("./routes/contact");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/contact", contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
