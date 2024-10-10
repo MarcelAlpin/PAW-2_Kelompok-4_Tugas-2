fafdaf53bb78da3e0147bc591527b5c0e938cfad
KennyLie26 <kennylie_2226240094@mhs.mdp.ac.id>
2024-10-10 16:00:31 +0700

    About

--- Tugas-2/app.js
+++ Tugas-2/app.js
@@ -8,6 +8,7 @@ const expressLayouts = require("express-ejs-layouts");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var aboutRouter = require("./routes/about");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/about", aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
