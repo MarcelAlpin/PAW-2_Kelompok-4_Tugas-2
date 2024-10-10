f7f1eff3d04c9910c76770c50ccff5fa77d225f5
jojoooook <jonathanchristian1221@gmail.com>
2024-10-10 18:24:52 +0700

    Update Homepage

--- Tugas-2/routes/index.js
+++ Tugas-2/routes/index.js
@@ -3,7 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index' , {title : 'Kopi Kopi', layout : 'main'});
+
+  const products = [
+    {
+      name: "Kopi Arabika",
+      description: "Rasa halus dan aroma yang menggoda.",
+      image: "/images/arabika.jpg",
+    },
+    {
+      name: "Kopi Robusta",
+      description: "Kekuatan rasa yang kuat dan penuh energi.",
+      image: "/images/robusta.jpg",
+    },
+    {
+      name: "Kopi Blend",
+      description: "Kombinasi sempurna dari berbagai jenis kopi.",
+      image: "/images/blend.jpg",
+    },
+  ];
+
+  const testimonials = [
+    {
+        message: 'Kopi Arabika dari sini adalah yang terbaik yang pernah saya coba! Aroma yang menggoda dan rasa halusnya membuat setiap tegukan menjadi pengalaman yang tak terlupakan. Saya sangat merekomendasikannya untuk para pecinta kopi!',
+        name: 'Joko',
+    },
+    {
+        message: 'Rasa kopi Robusta sangat kuat dan menggugah semangat! Kadar kafeinnya yang tinggi membuat saya merasa segar setiap kali meminumnya. Kopi ini sempurna untuk memulai hari saya dengan penuh energi!',
+        name: 'Sari',
+    },
+    {
+        message: 'Kopi Blend adalah favorit saya. Selalu memuaskan! Kombinasi berbagai jenis biji kopi memberikan rasa yang seimbang dan kompleks. Setiap kali saya menikmatinya, saya menemukan nuansa baru yang membuat saya ingin terus kembali.',
+        name: 'Diana',
+    },
+];
+
+
+  res.render('index' , {title : 'Kopi Kopi', products, testimonials, layout : 'main'});
 });
 
 module.exports = router;
