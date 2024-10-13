32596ed1bcad6a947eedc37f7939fe7c85535efe
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2024-10-13 21:46:27 +0700

    adding json for contact and about

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
   res.render("about", { title: "about", layout: "main" });
+});
+router.get("/json", function (req, res, next) {
+  res.json({
+    message: "About Us",
+    description:
+      "We are a coffee company dedicated to providing the best coffee experience.",
+    mission: "To bring the best coffee to every cup.",
+    vision: "To be the leading coffee brand in the world.",
+  });
 });
 module.exports = router;
--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET contact page. */
 router.get("/", function (req, res, next) {
   res.render("contact", { title: "Contact Us", layout: "main" });
+});
+
+/* GET contact JSON data. */
+router.get("/json", function (req, res, next) {
+  res.json({
+    message: "Contact Us",
+    email: "contact@kopikopi.com",
+    phone: "+1234567890",
+    address: "123 Coffee Street, Java City, 12345",
+  });
 });
 
 module.exports = router;
